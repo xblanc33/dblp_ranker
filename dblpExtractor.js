f64f2fc43974c9cd7ba9d27523a874035f07d37b
xblanc33 <blancxav@gmail.com>
2020-02-28 10:09:28 +0100

    improve search

--- dblpExtractor.js
+++ dblpExtractor.js
@@ -279,7 +279,8 @@ function cleanTitle(title) {
     let res = title;
     res.trim();
     res = res.toLowerCase();
-    res = res.replace(/\(\d*\)/g, '');
+    //res = res.replace(/\(\d*\)/g, '');
+    res = res.split('(')[0];
     res = res.split(',')[0];
     res = res.replace(':','');
     res = res.replace(/&amp;/g, '');
